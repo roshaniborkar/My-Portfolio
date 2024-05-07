3cbf80e29a436a411126f6c53907a20ba271e543
Roshani borkar <58977139+roshaniborkar@users.noreply.github.com>
2024-05-07 19:34:07 +0530

    CardItem.jsx

--- src/Components/CardItem.jsx
+++ src/Components/CardItem.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 const projects = [
   {
     pictureSrc:
-      "https://private-user-images.githubusercontent.com/58977139/326351991-5f0419c1-6b61-4718-a579-ab18ffdca9dd.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQzNzQ2ODUsIm5iZiI6MTcxNDM3NDM4NSwicGF0aCI6Ii81ODk3NzEzOS8zMjYzNTE5OTEtNWYwNDE5YzEtNmI2MS00NzE4LWE1NzktYWIxOGZmZGNhOWRkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MjklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDI5VDA3MDYyNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTI2NTc2MDU0NmExODBiNGUzZTU3ZWZjMDM5NmQ1ODgyZGE1NTkzYWRlN2U1OGMwNzQzMDkxNTU4NmVlOTNlYjMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.X89rfEPLltFxJlaiAWk72Jq1caAI_v07EvLLIqRALes",
+      "https://github.com/roshaniborkar/kidsZone/blob/main/images/Screenshot%202024-04-29%20123104.png?raw=true",
     title: "Kids Zone",
     description:
       "Kids Zone is a basic gaming and learning website designed for children. The website provides a safe and fun environment for kids to learn while they play. With a variety of games and activities, Kids Zone aims to make learning enjoyable for children. The website offers a wide range of activities, including educational games. as well as pictures with audio so it will help children develop skills and knowledge.",
@@ -21,7 +21,7 @@ const projects = [
     
   },
   {
-    pictureSrc: "https://private-user-images.githubusercontent.com/58977139/319247882-4173b82f-fb34-42c3-8ab7-db9dc5fb7906.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQzNzQ4MzIsIm5iZiI6MTcxNDM3NDUzMiwicGF0aCI6Ii81ODk3NzEzOS8zMTkyNDc4ODItNDE3M2I4MmYtZmIzNC00MmMzLThhYjctZGI5ZGM1ZmI3OTA2LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MjklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDI5VDA3MDg1MlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTc5ZTJhYTcyYTJiOTdiMjEwNzU0ZTg4ZmExMjk5NDMxOGI3MDk0NzgwMDllODE3MDE4NjBkYzMyMDdlYmIzNzgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.V-owAIne7U3hHg5rO7GUUhxWt3BmeWeDB0ldd_d4_4s",
+    pictureSrc: "https://img.lovepik.com/free-png/20220126/lovepik-girl-cooking-png-image_401764830_wh1200.png",
     title: "CareForU",
     description:
       "This dynamic Android app, crafted with Java, and SQlite, Used for forecommending home remedies based on diseases.",
@@ -29,7 +29,7 @@ const projects = [
   },
   {
     pictureSrc:
-      "https://github.com/manik-18/Personal-Portfolio/assets/102967918/ad75e85b-3d44-4066-ae62-cab045fd7489",
+      "https://github.com/roshaniborkar/My-Portfolio/blob/main/src/assets/Screenshot%202024-05-07%20192041.png?raw=true",
     title: "Personal Portfolio",
     description:
       "Explore my portfolio website crafted using React, Vite, and Tailwind CSS, where I proudly present a collection of my work. This platform not only showcases my projects but also highlights my diverse skill set, providing a comprehensive view of my capabilities.",
